b8cc39d3ad69e5b9e6b6bde05e0fd7c31a5b894c
SirVon <sirvon@loqoo.tv>
2013-12-28 10:50:21 +0000

    turn newly created channel/network queue into a push queue and add subscriber/processing code to new channel/network queue

--- newQueueCreated/newQueueCreated.py
+++ newQueueCreated/newQueueCreated.py
@@ -31,7 +31,7 @@ incomingUrl = urlparse(u).query
 print incomingUrl
 ok = parse_qsl(urlparse(u).query, keep_blank_values=True)
 print ok
-print ok.eventType
+print (ok[0]0])
 eventT = q['eventType']
 print eventT
 networkName = "o"
