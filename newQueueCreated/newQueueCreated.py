fbd7f6b2798e1f9d5b4c9c4518b90c4903806cc1
SirVon <sirvon@loqoo.tv>
2013-12-28 09:20:11 +0000

    turn newly created channel/network queue into a push queue and add subscriber/processing code to new channel/network queue

--- newQueueCreated/newQueueCreated.py
+++ newQueueCreated/newQueueCreated.py
@@ -4,18 +4,10 @@ from requests import *
 from iron_mq import *
 import urlparse, os
 
-        def addSubscriberToQueue(self, queue, networkName, subscriber):
-                url = queue
-		d = {'push_type': "multicast", 'error_queue': networkName+'Error', 'subscribers': [{"url": subscriberPushSceneToChannel}] }
-                r = requests.delete(url, data=d)
-
-IRON_TOKEN = os.getenv('IRON_TOKEN')
-ironmq = IronMQ(host="mq-aws-us-east-1.iron.io",
-                project_id="52ba6fcb4c05a60009000001",
-                token=IRON_TOKEN,
-                protocol="https", port=443,
-                api_version=1,
-                config_file=None)
+def addSubscriberToQueue(self, queue, networkName, subscriber):
+	url = queue
+	d = {'push_type': "multicast", 'error_queue': networkName+'Error', 'subscribers': [{"url": subscriberPushSceneToChannel}] }
+	r = requests.post(url, data=d)
 
 payload = None
 payload_file = None
@@ -32,11 +24,16 @@ except ValueError:
         pass
         
 u = contents
+print u
 incomingUrl = urlparse(u)
 print incomingUrl
 q = incomingUrl.parse_qs
-print q
-print q['eventType']
+eventT = q['eventType']
+print eventT
+if eventT == "newChannelQueueCreated":
+    print "ok"
+else:
+    print "ko"
 //get networkName
 queue = 'https://mq-aws-us-east-1.iron.io/projects/1/52ba6fcb4c05a60009000001/queues/%s?oauth=rhJuFZcZzPqj48eR471VvDu1O40'%s networkName
 subscriberPushSceneToChannel = 'https://worker-aws-us-east-1.iron.io:443/2/projects/52ba6fcb4c05a60009000001/tasks/webhook?code_name=pushSceneToChannel&oauth=rhJuFZcZzPqj48eR471VvDu1O40'
